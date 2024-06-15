bc759577c12343d5af177e05f7284460bedee401
yermek <ye.koishibekov@niikmg.kz>
2024-06-16 03:04:53 +0500

    016

---
+++ start.sh
@@ -0,0 +1,10 @@
+cd ~
+mkdir -p proxmox-homelab/ssh
+cd proxmox-homelab/ssh
+wget --backups=1 https://raw.githubusercontent.com/koye64x/proxmox-homelab/main/ssh/id_rsa.pub
+cd ~
+mkdir -p proxmox-homelab/proxmox
+cd proxmox-homelab/proxmox
+wget --backups=1 https://raw.githubusercontent.com/koye64x/proxmox-homelab/main/proxmox/create-vm-ubuntu-template.sh
+chmod +x create-vm-ubuntu-template.sh
+./create-vm-ubuntu-template.sh
